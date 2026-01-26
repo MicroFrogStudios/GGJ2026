95e89fc2c20c0473732f8a10fea54bb6ace43412
cmarah <carlos@marah.dev>
2026-01-27 00:20:24 +0100

    Center ripple on character

--- scripts/ripple_layer.gd
+++ scripts/ripple_layer.gd
@@ -6,6 +6,17 @@ extends ColorRect
 
 
 func _on_player_mask_change(_new_mask_number: int) -> void:
+
+	# Get normalized char position & set center
+	var player_transform: Transform2D = Character.get_global_transform_with_canvas()
+	var player_viewport_pos = player_transform.origin
+	var viewport_size: Vector2 = get_viewport().size
+	var normalized_char_pos: Vector2 = Vector2(
+		4 * player_viewport_pos.x / viewport_size.x,
+		4 * player_viewport_pos.y / viewport_size.y,
+	)
+	material.set_shader_parameter("center", normalized_char_pos)
+
 	if EffectsAnimator.is_playing():
 		EffectsAnimator.seek(0.0)
 		return
