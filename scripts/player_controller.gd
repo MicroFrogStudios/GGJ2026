e6111320467b9ac05faf5fcf28159b1b4e2ec474
meskion <manufdh@hotmail.com>
2026-01-27 00:52:05 +0100

    added exports to new movement variables

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -8,10 +8,14 @@ extends CharacterBody2D
 
 
 @export var speed := 150.0
-@export var jump_velocity = -200.0
+@export var jump_velocity = -300.0
 @export var initial_num_masks := 3
 var is_jumping = false
-var acceleration := 100.0
+@export var jump_deccel := 100.0
+@export var braking_speed = 15
+@export var start_accel = 10
+
+
 signal change_mask(new_mask_number: int)
 signal mask_acquired(acquired_mask_number: int)
 signal death()
@@ -81,7 +85,7 @@ func _physics_process(delta: float) -> void:
 		anim.play("jumping")
 		
 	if Input. is_action_just_released("jump") and is_jumping and velocity.y < 0:
-		velocity.y = clamp(velocity.y + acceleration,jump_velocity,0) 
+		velocity.y = clamp(velocity.y + jump_deccel,jump_velocity,0) 
 		is_jumping = false
 		
 	## Sideways movement
@@ -90,14 +94,13 @@ func _physics_process(delta: float) -> void:
 		anim.play("run")
 		anim.flip_h = false
 		anim.flip_h = direction_x < 0
-		velocity.x = move_toward(velocity.x,direction_x * speed,acceleration/5)
+		velocity.x = move_toward(velocity.x,direction_x * speed, start_accel)
 		anim.speed_scale = velocity.x/100
 	else:
 		if velocity.x != 0:
-			#anim.flip_h = direction_x > 0
 			anim.play("braking")
 		else: anim.play("idle")
-		velocity.x = move_toward(velocity.x,0, speed/15)
+		velocity.x = move_toward(velocity.x,0, braking_speed)
 
 	move_and_slide()
 
