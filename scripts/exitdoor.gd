4158265fd72185c956653d6041f770fea5558669
cmarah <carlos@marah.dev>
2026-01-26 20:39:11 +0100

    Some more code tweaks

--- scripts/exitdoor.gd
+++ scripts/exitdoor.gd
@@ -1,12 +1,7 @@
 extends Node2D
 
 
-@export var next_scene : String
-
-
 func _on_door_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "PlayerCharacter":
 		print("Player has reached the exit door!")
 		gc.load_next_scene()
-
-		
--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,18 +1,32 @@
 extends Node
-var current_scene :LoadedScene
 
 
 @export var editor_scenes :Array[PackedScene]
+
+
+class UnloadedScene:
+	var scene : PackedScene
+	var scene_name : String
+
+
+class LoadedScene:
+	var index : int
+	var scene : Node
+	var scene_name : String
+
+	func _init(scene_to_load :UnloadedScene,i:int) -> void:
+		self.index = i
+		scene_name = scene_to_load.scene_name
+		scene = scene_to_load.scene.instantiate()
+
+
 var unloaded_scenes : Array[UnloadedScene]
+var current_scene : LoadedScene
 
 
 func load_next_scene():
 	print("changing from scene ",current_scene.scene_name, " to ", unloaded_scenes[(current_scene.index+1) % unloaded_scenes.size()].scene_name )
 	load_scene_by_index((current_scene.index+1) % unloaded_scenes.size())
-
-
-func reload_scene():
-	pass
 
 
 func _ready() -> void:
@@ -35,19 +49,3 @@ func load_scene_by_index( i :int):
 		current_scene = null
 	current_scene = LoadedScene.new(unloaded_scenes[i],i)
 	add_sibling.call_deferred(current_scene.scene)
-
-
-class UnloadedScene:
-	var scene : PackedScene
-	var scene_name : String
-
-
-class LoadedScene:
-	var index : int
-	var scene : Node
-	var scene_name : String
-
-	func _init(scene_to_load :UnloadedScene,i:int) -> void:
-		self.index = i
-		scene_name = scene_to_load.scene_name
-		scene = scene_to_load.scene.instantiate()
