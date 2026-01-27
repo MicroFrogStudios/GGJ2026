83018e3e524a1e15553f17ca9fe5e9df1a44a522
Carlos <carlos@marah.dev>
2026-01-27 15:37:40 +0100

    Ignore unused signal warnings

--- scripts/state_machine.gd
+++ scripts/state_machine.gd
@@ -1,26 +1,29 @@
 class_name StateMachine
 extends Node
 
+
 @export var initial_state : State
+
 
 @onready var parent :Node2D = get_parent()
 
-var current_state : State
 
+var current_state : State
 var states : Dictionary = {}
+
+
 func on_state_transition(state_name : String):
 	_disconnect()
 	current_state.Exit()
-	print("transition from " + current_state.Name() + " to: " + state_name)
+	print("transition from " + current_state.Name() + " to " + state_name)
 	current_state = states[state_name]
 	_connect()
 	current_state.Enter()
 
 
 func _ready() -> void:
-	current_state = initial_state	
+	current_state = initial_state
 	_connect()
-	
 	for child in get_children():
 		if child is State:
 			states[child.Name()] = child
