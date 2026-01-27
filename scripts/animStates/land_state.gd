b9a9c79dda5efeb3f21146d3876d7c527ac9f1f5
meskion <manufdh@hotmail.com>
2026-01-27 23:33:17 +0100

    changes to jump behaviour

--- scripts/animStates/land_state.gd
+++ scripts/animStates/land_state.gd
@@ -24,7 +24,7 @@ func Update(_delta: float):
 func Physics_Update(_delta: float):
 	if player.velocity.x != 0:
 		transitioned.emit(RunState.Name())
-	if Input.is_action_just_pressed("jump") and player.is_on_floor() and not player.control_disabled:
+	if Input.is_action_pressed("jump") and player.is_on_floor() and not player.control_disabled:
 		transitioned.emit(JumpState.Name())
 
 
