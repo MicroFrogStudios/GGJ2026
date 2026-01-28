6752144fa180be3eb23d43656d9135569cfaa366
cmarah <carlos@marah.dev>
2026-01-28 01:01:47 +0100

    Make mask item float & generalize

--- scripts/mask_fragment.gd
+++ scripts/mask_fragment.gd
@@ -2,12 +2,20 @@ extends Node2D
 
 
 @export var mask_number = 1
+@export var mask_texture : Texture2D
 
 
 @onready var mask_collision_shape: CollisionShape2D = $MaskArea2D/CollisionShape2D
 
 
+var initial_position: Vector2
+
+
 signal got_mask(mask_number: int)
+
+
+func _ready() -> void:
+	initial_position = position
 
 
 func disable_mask() -> void:
@@ -21,3 +29,9 @@ func _on_mask_area_2d_body_entered(body: Node2D) -> void:
 		print("Player has gotten mask!")
 		got_mask.emit(mask_number)
 		call_deferred("disable_mask")
+
+
+func _physics_process(_delta: float) -> void:
+	var time = Time.get_ticks_msec()
+	var variance = sin(time / 400.0) * Vector2(0, 3)
+	position = initial_position + variance
