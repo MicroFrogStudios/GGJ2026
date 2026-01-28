c256ead40e80913bb6588ae6a299fbeb61738aeb
cmarah <carlos@marah.dev>
2026-01-28 17:08:47 +0100

    Try to reduce camera glitch

--- scripts/camera_follow.gd
+++ scripts/camera_follow.gd
@@ -2,9 +2,8 @@ extends Camera2D
 
 
 @export var following :Node2D
-@export var follow_speed := 100
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if following.position.y < 100:
-		position = position.lerp(following.position, follow_speed*delta)
+		position = following.position
