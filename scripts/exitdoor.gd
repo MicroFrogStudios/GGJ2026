d06d1f32e5148dc0b1db4a02aa2783f5ddc02dfc
meskion <manufdh@hotmail.com>
2026-01-28 21:12:24 +0100

    partiiculas

--- scripts/exitdoor.gd
+++ scripts/exitdoor.gd
@@ -7,7 +7,8 @@ signal player_reached_exit()
 func _on_door_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "PlayerCharacter":
 		player_reached_exit.emit()
-
+		$victory_particles.emitting = true
+		$victory_particles2.emitting = true
 
 func go_to_next_level() -> void:
 	gc.load_next_scene()
