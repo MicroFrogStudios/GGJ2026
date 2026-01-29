c63864f37c657eaf369e89f0b2c39695f149eaab
meskion <manufdh@hotmail.com>
2026-01-29 13:47:08 +0100

    chandelier platform can be in mask layers

--- scripts/chandelier_plataform.gd
+++ scripts/chandelier_plataform.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+@export var mask_layer = 0
+
+@onready var collision :CollisionShape2D = $AnimatableBody2D/CollisionShape2D
+@onready var sprite: TileMapLayer = $AnimatableBody2D/TileMapLayer
+@onready var player :PlayerCharacter = %PlayerCharacter
+
+func _ready() -> void:
+	player.change_mask.connect(on_mask_change)
+	call_deferred("on_mask_change",player.mask)
+	
+func on_mask_change(mask:int):
+	sprite.enabled = mask == mask_layer
+	collision.call_deferred("set_disabled", not sprite.enabled)
+		
