413dc9332efec9896d19d59beedd2b1d195a0d8d
meskion <manufdh@hotmail.com>
2026-01-31 00:47:42 +0100

    button nav on menus and new assets

--- scripts/in_game_menu.gd
+++ scripts/in_game_menu.gd
@@ -7,6 +7,8 @@ func _on_continue_button_pressed() -> void:
 	print("continue")
 	gc.close_in_game_menu()
 	
+func gain_focus():
+	$Panel/Main/ContinueButton.grab_focus.call_deferred()
 
 func _on_quitbutton_pressed() -> void:
 	print("quit")
@@ -16,7 +18,9 @@ func _on_quitbutton_pressed() -> void:
 func _on_settingsbutton_pressed() -> void:
 	$Panel/Settings.visible = true
 	$Panel/Main.visible = false
+	$Panel/Settings/volume/HSlider.grab_focus.call_deferred()
 
 func _on_quit_settings_pressed() -> void:
 	$Panel/Settings.visible = false
 	$Panel/Main.visible = true
+	$Panel/Main/ContinueButton.grab_focus.call_deferred()
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,11 @@
 extends Control
 
-
+@onready var start_button = $menu/startbutton
+@onready var slider_settings = $Settings/volume/HSlider
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$menu/startbutton.grab_focus.call_deferred()
-	pass
+	start_button.grab_focus.call_deferred()
+	
 
 
 func _on_startbutton_pressed() -> void:
@@ -15,11 +16,14 @@ func _on_startbutton_pressed() -> void:
 func _on_quitbutton_pressed() -> void:
 	get_tree().quit()
 	
+	
 func _on_settingsbutton_pressed() -> void:
 	$menu.visible = false;
 	$Settings.visible = true
+	slider_settings.grab_focus.call_deferred()
 
 
 func _on_quit_settings_button_pressed() -> void:
 	$menu.visible = true;
 	$Settings.visible = false
+	start_button.grab_focus.call_deferred()
