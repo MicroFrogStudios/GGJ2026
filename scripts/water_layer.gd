852206e4873472e8ee995a37cb7aa7fc4b8fc623
cmarah <carlos@marah.dev>
2026-01-30 14:04:19 +0100

    Add second water layer

--- scripts/water_layer.gd
+++ scripts/water_layer.gd
@@ -1,6 +1,9 @@
 extends TileMapLayer
 
 
+@export var is_bg := false
+
+
 var base_position : Vector2
 
 
@@ -12,6 +15,7 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	var x_disp = cos(Time.get_ticks_msec() / period_modifier) * 8.0
-	var y_disp = sin(Time.get_ticks_msec() / period_modifier) * 3.0
+	var period_offset = 0.0 if not is_bg else period_modifier / 2.0
+	var x_disp = cos(Time.get_ticks_msec() / period_modifier + period_offset) * 8.0
+	var y_disp = sin(Time.get_ticks_msec() / period_modifier + period_offset) * 3.0
 	position = base_position + Vector2(x_disp, y_disp)
