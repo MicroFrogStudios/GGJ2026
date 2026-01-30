6be1172bf23c2d24d9d47b9be09bac151e12a83b
meskion <manufdh@hotmail.com>
2026-01-30 14:50:37 +0100

    more support for moving platforms, more decor

--- scripts/vertical_moving_plataform.gd
+++ scripts/vertical_moving_plataform.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 
 @export var mask_layer : int = 0
-
+@export var movement_offset := 0
 
 @onready var collision : CollisionShape2D = $CharacterBody2D/CollisionShape2D
-@onready var sprite : Sprite2D = $CharacterBody2D/Sprite2D
+@onready var sprite : Sprite2D = $CharacterBody2D/CollisionShape2D/Sprite2D
 @onready var player : PlayerCharacter = %PlayerCharacter
-@onready var chain : TileMapLayer = $CharacterBody2D/Chain
+@onready var chain : TileMapLayer = $CharacterBody2D/CollisionShape2D/Chain
 
 var initial_y_position : float = 0.0
 
@@ -26,6 +26,5 @@ func on_mask_change(mask:int):
 
 
 func _physics_process(_delta: float) -> void:
-	collision.position.y = initial_y_position + sin(Time.get_ticks_msec() / 500.0) * 10.0
-	sprite.position.y = collision.position.y - 15.0
-	chain.position.y = collision.position.y + 20.0
+	collision.position.y = initial_y_position + sin(Time.get_ticks_msec() / 500.0 + movement_offset) * 10.0
+	
