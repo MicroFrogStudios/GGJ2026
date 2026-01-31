f37e39f7e9e20cc4ccd7ac64fd82b8e95764fa20
meskion <manufdh@hotmail.com>
2026-01-31 18:44:10 +0100

    credit scene functional

---
+++ scripts/credits.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var quit_button = $menu/quitbutton
+
+func _on_quitbutton_pressed() -> void:
+	gc.load_main_menu()
+
+func _ready() -> void:
+	quit_button.grab_focus.call_deferred()
