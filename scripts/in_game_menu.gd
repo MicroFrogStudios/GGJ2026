01de83b14a4e4908c86ecf04072be5cfbee2fb26
cmarah <carlos@marah.dev>
2026-01-31 02:49:48 +0100

    Better pause menu

--- scripts/in_game_menu.gd
+++ scripts/in_game_menu.gd
@@ -1,24 +1,30 @@
 extends Control
 
+
 func _ready() -> void:
 	gc.in_game_menu_instance = self
+
 
 func _on_continue_button_pressed() -> void:
 	print("continue")
 	gc.close_in_game_menu()
-	
+
+
 func gain_focus():
 	$Panel/Main/ContinueButton.grab_focus.call_deferred()
 
-func _on_quitbutton_pressed() -> void:
+
+func _on_quit_button_pressed() -> void:
 	print("quit")
 	gc.close_in_game_menu()
 	gc.load_main_menu()
+
 
 func _on_settingsbutton_pressed() -> void:
 	$Panel/Settings.visible = true
 	$Panel/Main.visible = false
 	$Panel/Settings/volume/HSlider.grab_focus.call_deferred()
+
 
 func _on_quit_settings_pressed() -> void:
 	$Panel/Settings.visible = false
