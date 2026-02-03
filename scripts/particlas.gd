80e2c5d7cfb3c1e256fac3aab772c426dfa79a02
cmarah <carlos@marah.dev>
2026-02-03 01:15:54 +0100

    Heavy restructuring

--- scripts/particlas.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-@export var mask_layer : int = 0
-
-
-func on_mask_change(mask:int):
-	visible = mask != mask_layer
---
+++ ui/game_ui.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+@export var player_character : PlayerCharacter
+@export var ui_mask_pos : Array[float] 
+
+@onready var selector : TextureRect = $mask_selector
+@onready var _current_mask = player_character.mask
+
+
+func _ready() -> void:
+	player_character.change_mask.connect(_on_mask_changed)
+	player_character.mask_acquired.connect(on_mask_accquired)
+	if player_character.num_masks == 0:
+		visible= false
+
+
+func on_mask_accquired(_acquired_mask_number:int):
+	visible = true
+
+
+func _on_mask_changed(mask : int):
+	_current_mask = mask
+
+
+func _process(_delta: float) -> void:
+	selector.position.x = move_toward(selector.position.x, ui_mask_pos[_current_mask],5)
